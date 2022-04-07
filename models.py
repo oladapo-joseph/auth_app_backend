caf1c32ea604e467aeb679168967011ba442a4d9
Adeleke Oladapo Joseph <oladapo_joseph@yahoo.com>
2022-04-07 06:23:27 +0100

    authentication app

---
+++ models.py
@@ -0,0 +1,17 @@
+from app import db, create_app
+from flask_login import UserMixin
+
+
+
+db.create_all(app =create_app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(1000))
+    
+
+
+
